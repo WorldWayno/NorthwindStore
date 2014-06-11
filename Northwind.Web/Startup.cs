b6bb62c14c371571799af4de05a669c30030ef9b
wayne <world.wayno@gmail.com>
2014-06-10 23:36:13 -0400

    adding Northwind.Web

---
+++ Northwind.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Northwind.Web.Startup))]
+namespace Northwind.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
