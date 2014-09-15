924894d33c8e3f3cbca6919a83133cb04d83790d
wayne <world.wayno@gmail.com>
2014-09-14 23:45:46 -0400

    more swagger ui changes

---
+++ Northwind.Web/Scripts/theme/init.js
@@ -0,0 +1,30 @@
+﻿
+$(document).ready(function () {
+
+    var s = skrollr;
+    var sActive = false;
+
+    if ($(window).width() > 1024) {
+        s.init();
+        sActive = true;
+    }
+
+    $(window).on('resize', function () {
+        if ($(window).width() < 1024 && sActive) {
+            s.init().destroy();
+            sActive = false;
+        }
+        else if ($(window).width() > 1024) {
+            s.init();
+            sActive = true;
+        }
+    });
+
+    /*===================================================================================*/
+    /*	STICKY NAVIGATION
+    /*===================================================================================*/
+    $('.navbar .navbar-collapse').waypoint('sticky');
+
+});
+
+
